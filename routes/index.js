e089d4ee51cf5c7ad9ce91bc4082578719ac998a
Matthew Bouchard <bouchard.matthewj@gmail.com>
2016-04-01 11:39:37 -0600

    add gear table with foreign key

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    knex('users')
+    .where({id: req.session.userID})
+    .then(function(response){
+        knex('gear')
+        .
+    })
   res.render('index', { title: 'Express' });
 });
 
